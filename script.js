2417119b097912d2b801d0f314ff773b52fe458f
Gabriel Medeiros <g4brielmedeiros@pm.me>
2021-11-01 14:19:02 -0300

    Optimized and improved script.

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
 console.log('hey')
+const moves = ['ROCK', 'PAPER', 'SCISSORS'];
 
 function getComputerMove() {
-    //DONE: Randomly return Rock/Paper/Scissors
-
-    let moves = ['ROCK', 'PAPER', 'SCISSORS'];
+    // Randomly return Rock/Paper/Scissors
     let random = Math.floor(Math.random()*3)
     return moves[random];
 }
 
 function playRound(playerSel, computerSel) {
-    // compares player's moves
-    // returns a string declaring the winner "You Lose! Paper beats Rock"
-    // parameter must be case insensitive.
+    // Compares player's moves
+    // Returns a string declaring the winner "You Lose! Paper beats Rock"
+    // Parameter must be case insensitive. 
 
     let player = playerSel.toUpperCase();
     let computer = computerSel.toUpperCase();
     let message = `${player} vs. ${computer}...`
+
+    function versus(move) {
+        return computer == move ? `${message} Computer Wins!` : `${message} You win!`
+    }
 
     if (player == computer) return `${message} It's a tie!`
 
     switch(player) {
         
         case 'ROCK':
-            return (computer == 'PAPER' ? `${message} Computer Wins!` : `${message} You win!`);
+            return versus('PAPER');
             break;
         case 'PAPER':
-            return (computer == 'SCISSORS' ? `${message} Computer Wins!` : `${message} You win!`);
+            return versus('SCISSORS');
             break;
         case 'SCISSORS':
-            return (computer == 'ROCK' ? `${message} Computer Wins!` : `${message} You win!`);
+            return versus('ROCK');
             break;
     }
+}
+
+function isValid(move) {
+    let m = move.toUpperCase();
+    return m == 'ROCK' || m == 'PAPER' || m == 'SCISSORS' 
 }
 
 function game(rounds) {
@@ -38,11 +46,22 @@ function game(rounds) {
     // Keeps score and reports a winner at the end.
     // Maybe use loops
 
-
     for (i = 0; i<rounds; i++) {
-        let you = prompt('Rock, Paper, or Scissors?');
-        let cpu = getComputerMove();
-        console.log(playRound(you, cpu));
+        let you = ''; 
+        
+        you = prompt('Rock, Paper, or Scissors?');
+
+        while(!isValid(you)) {
+            you = prompt('I said ROCK, PAPER, or SCISSORS?');
+
+        }
+
+        if (isValid(you)) {
+            console.log(you)
+            alert(playRound(you, getComputerMove()));
+        }
+
+        
     }
 }
 
@@ -53,7 +72,5 @@ function game(rounds) {
 
 
 //TESTS
-
-
-game(2)
+game(1)
 
