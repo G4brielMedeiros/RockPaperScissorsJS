45a86175c968410c2d4a00d33e0c10ebf1957811
Gabriel Medeiros <g4brielmedeiros@pm.me>
2021-11-01 04:39:25 -0300

    TODO comments added.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('hey')
+
+function computerPlay() {
+    //TODO: Randomly return Rock/Paper/Scissors
+}
+
+function playRound(playerSel, comSel) {
+    //TODO: compares player's moves
+    //      returns a string declaring the winner "You Lose! Paper beats Rock"
+    //      parameter must be case insensitive.
+}
+
+function game() {
+    //TODO: Play 5 rounds of the game.
+    //      Keeps score and reports a winner at the end.
+    //      Maybe use loops?
+}
+
+    //  Output with Console.log()
+    //  Input with prompt()
