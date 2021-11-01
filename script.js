97bf3faa5ae642d11aeccda0014b8635185b4ad4
Gabriel Medeiros <g4brielmedeiros@pm.me>
2021-11-01 16:49:17 -0300

    Refactored script

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 // List of valid moves.
 const moves = ['ROCK', 'PAPER', 'SCISSORS'];
 
-// Returns random move from moves.
-const getComputerMove = () => { moves[Math.floor(Math.random()*3)] }
+// Returns random move from [moves].
+const getComputerMove = () => moves[Math.floor(Math.random()*3)]
 
-// Checks if a move is valid.
-const isValid = move =>     { return moves.includes(move.toUpperCase()) }
+// Checks if a move is in [moves].
+const isValid = move => moves.includes(move.toUpperCase())
 
 
 // Compares player's moves,
 // returns a string declaring the winner.
 function playRound(playerMove, compMove) {
 
-
     let player = playerMove.toUpperCase();
     let computer = compMove.toUpperCase();
 
     let message = `${player} vs. ${computer}...`
 
-    let versus = (move) => 
-        { computer == move ? `${message} Computer Wins!` : `${message} You win!` }
+    let versus = (move) => computer == move ? `${message} Computer Wins!` : `${message} You win!`
     
-
     if (player == computer) return `${message} It's a tie!`
 
     switch(player) {
-        
+
         case 'ROCK':
             return versus('PAPER');
+
             break;
         case 'PAPER':
             return versus('SCISSORS');
@@ -39,29 +37,27 @@ function playRound(playerMove, compMove) {
 }
 
 
+// Plays the game [rounds] times.
+// Keeps score and reports a winner at the end.
 function game(rounds) {
-    // Play 5 rounds of the game.
-    // Keeps score and reports a winner at the end.
-    // Maybe use loops
+    
+    let playScore = 0;
+    let compScore = 0;
 
     for (i = 0; i<rounds; i++) {
-        let you = ''; 
         
+        let you = ''; 
         you = prompt('Rock, Paper, or Scissors?');
 
-        while(!isValid(you)) {
-            you = prompt('I said ROCK, PAPER, or SCISSORS?');
+        while(!isValid(you)) { you = prompt('I said ROCK, PAPER, or SCISSORS?'); }
 
-        }
+        console.log(you)
 
-        if (isValid(you)) {
-            console.log(you)
-            alert(playRound(you, getComputerMove()));
-        }
+        let comp = getComputerMove();
 
-        
+        alert(playRound(you,comp))    
     }
 }
 
-game(1)
+game(5)
 
